048a1d4d302889605cbda8134e0102706ccb8bb2
Evgeniy <avtovo1234@gmail.com>
2022-12-06 21:17:06 +0300

    Carousel

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('.carousel__inner').slick({
+        speed: 1200,
+        adaptiveHeight: false,
+        prevArrow: '<button type="button" class="prev"><img src="img/images/left.png"></img></button>',
+        nextArrow: '<button type="button" class="next"><img src="img/images/right.png"></img></button>'
+    });
+});
+
